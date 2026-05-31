17d9a90f7061aa3e0a0e8c561272188ae1141758
amplitudemodulada <amplitudemodulada@gmail.com>
2026-05-31 13:01:30 -0300

    fix: debug deployment

--- api/index.js
+++ api/index.js
@@ -1,2 +1,13 @@
-const app = require('../src/server');
-module.exports = app;
+let loaded = false;
+let loadError = null;
+
+try {
+  const app = require('../src/server');
+  loaded = true;
+  module.exports = app;
+} catch (err) {
+  loadError = { message: err.message, stack: err.stack, code: err.code };
+  module.exports = (req, res) => {
+    res.status(500).json({ error: 'require failed', details: loadError });
+  };
+}
